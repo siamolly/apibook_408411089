c799b476a7f365385e7b6c44aedb003bc6cea3b6
siamolly <408411089@gms.tku.edu.tw>
2021-06-09 19:31:10 +0800

    Route for GET /apiGroup, GET /apiGroup/add

--- routes/apiGroup.js
+++ routes/apiGroup.js
@@ -1,20 +1,31 @@
 const express = require('express');
+const fetch = require('node-fetch');
 const router = express.Router();
 
 //GET all data
 router.get('/', async function (req, res, next) {
     let data;
     try {
-      const [rows] = await db.query('SELECT * FROM company ORDER BY id desc');
-      data = rows;
-      // res.json(data);
-      res.render('apiGroup', { data });
+      const response = await fetch('http://localhost:1337/group');
+      const data = await response.json();
+      
+      res.render('apiGroup/index', { data });
     } catch (err) {
       console.log('Errors on getting books!');
-      res.render('group', { data:data });
+      res.render('apiGroup/index', { data:'' });
     }
   });
+  
+// display add book page
+router.get('/add', async function (req, res, next) {
+  // res.send('display add book page')
 
+  res.render('apiGroup/add',{
+    group:'',
+    company:'',
+    id:'',
+  });
+});
 
 
 module.exports = router;
